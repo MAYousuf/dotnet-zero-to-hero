a1b974188f9253ae4a5eab1279a8902742f97c4a
Mohamed Yousuf <myousuf@tetco.sa>
2024-12-02 14:15:10 +0300

    add

---
+++ 005-validation-with-mediatr-pipeline-behavior-and-fluentvalidation/MediatRPipelineFluentValidation/test.cs
@@ -0,0 +1,24 @@
+﻿using FluentResults;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace MediatRPipelineFluentValidation;
+
+public static class ValidationExtensions
+{
+    public static Result WithValidationResults(this Result result, List<ValidationFailure> failures)
+    {
+        foreach (var failure in failures)
+        {
+            var reason = failure.MapToReason(); 
+            result.Reasons.Add(reason);
+        }
+
+        return result;
+    }
+
+    public static IReason MapToReason(this ValidationFailure failure)
+    {
+        return new Error(failure.ErrorMessage);
+    }
+}
